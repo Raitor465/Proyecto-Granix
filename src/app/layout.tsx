45e1a2bd99a363a638bb825340b7a0982512eca1
Mauro Grageda <mauro.grageda@uap.edu.ar>
2024-09-23 17:51:38 -0300

    arreglando error en la pagina.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import React, { ReactNode } from 'react';
 
 interface LayoutProps {
@@ -7,17 +7,19 @@ interface LayoutProps {
 
 export default function Layout({ children }: LayoutProps) {
   return (
-    <html lang="en"> {/* Asegúrate de incluir la etiqueta <html> */}
+    <html lang="en">
       <head>
-        <title>Granix</title> {/* Puedes agregar otros elementos aquí como meta tags */}
+        <title>Granix</title>
+        {/* Puedes agregar otros elementos aquí como meta tags */}
       </head>
       <body>
         <h1>Granix</h1> {/* Encabezado de la app */}
         {children} {/* Aquí se renderizarán las páginas hijas */}
+        <footer>
+          <p>Hola</p>
+        </footer>
       </body>
-      <footer>
-        <p>Hola</p>
-      </footer>
     </html>
   );
 }
+
