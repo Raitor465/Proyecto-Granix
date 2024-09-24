9c1bf858ca5b7b832d80ecb93bec858a75d0b161
YahirAedo <alejandro.aedo@uap.edu.ar>
2024-09-23 21:05:08 -0300

    Comienzo de diseño de Ruta de Visita

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 "use client";
 import React, { ReactNode } from 'react';
+import "../styles/globals.css";
 
 interface LayoutProps {
   children: ReactNode;
@@ -12,14 +13,22 @@ export default function Layout({ children }: LayoutProps) {
         <title>Granix</title>
         {/* Puedes agregar otros elementos aquí como meta tags */}
       </head>
-      <body>
-        <h1>Granix</h1> {/* Encabezado de la app */}
-        {children} {/* Aquí se renderizarán las páginas hijas */}
-        <footer>
-          <p>Hola</p>
+      <body className="bg-white text-gray-800">
+        <header className="flex justify-between items-center p-4 bg-[#AA2DB3] text-white">
+          <h1 className="text-2xl font-bold">Granix</h1>
+          <div>
+            <img src="/path/to/logo.png" alt="Logo" className="h-10" /> {/* Reemplaza con la ruta de tu logo */}
+          </div>
+        </header>
+
+        <main className="p-4">
+          {children} {/* Aquí se renderizarán las páginas hijas */}
+        </main>
+
+        <footer className="bg-gray-800 text-white text-center p-4">
+          <p>&copy; {new Date().getFullYear()} Granix. Todos los derechos reservados.</p>
         </footer>
       </body>
     </html>
   );
 }
-
