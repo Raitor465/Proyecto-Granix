dbe14d6997e61372cf379feb53b6a8b094970472
Mauro Grageda <mauro.grageda@uap.edu.ar>
2024-11-18 10:38:31 -0300

    agregando menu

---
+++ src/app/menu/layout.tsx
@@ -0,0 +1,8 @@
+export default function MenuLayout({ children }: { children: React.ReactNode }) {
+    return (
+      <div>
+        <h1 className="text-2xl font-bold text-center">Menu</h1>
+        <div>{children}</div>
+      </div>
+    );
+  }
